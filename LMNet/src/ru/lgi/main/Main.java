019d419ddb469c4e0b32a8a0ebc15ba1a22b2387
LaughingMan <laughingmaan@icloud.com>
2016-03-17 11:15:22 +0300

    Initial commit

---
+++ LMNet/src/ru/lgi/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ru.lgi.main;
+
+/**
+ * @author LaughingMan
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
